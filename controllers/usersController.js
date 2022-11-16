1bb503e8ebd33e1e9e6bb7d2aa9f609f80647bb9
Ali Eldeba <alieldeba375@gmail.com>
2022-11-16 18:28:24 +0200

    Update usersController.js

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -46,7 +46,7 @@ const addUser = async (req, res) => {
     res.status(201).json(user);
   } catch (error) {
     const errors = handleErrors(error);
-    res.json({ errors });
+    res.status(400).json({ errors });
   }
 };
 
